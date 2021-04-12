0e2017004df2d0f2b65af26ea647c916b3e7b26b
braulioo94 <brauliot.salazar@hotmail.com>
2021-04-12 13:49:57 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React,{useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png' ;
+import Formulario from './componentes/Formulario' ;
+import axios from 'axios' ;
+import Cotizacion from './componentes/Cotizacion' ;
+
+
+const Contenedor = styled.div`
+  max-width: 900px ;
+  margin: 0 auto ;
+  @media (min-width:992px) {
+    display:grid ;
+    grid-template-columns: repeat( 2 , 1fr) ;
+    column-gap: 2rem ;
+  }
+`;
+
+const Imagen = styled.img `
+  max-width: 100% ;
+  margin-top: 5rem ;
+`;
+
+const Heading = styled.h1`
+  font-family:'Bebas Neue', cursive ;
+  color: #FFF;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px ;
+  margin-top:80px;
+
+  &::after{
+    content:'';
+    width:100px;
+    height:6px ;
+    background-color:#66a2fe;
+    display:block;
+  } 
+
+
+`;
+
+
+
+function App() {
+
+  const [moneda, guardarMoneda] = useState('') ;
+  const [criptomoneda, guardarCriptomoneda] = useState('')
+  const [resultado, guardarResultado] = useState({}) ;
+
+
+  useEffect(() => {
+    const cotizadarCriptomoneda = async () => {
+
+      if(moneda === '') return ;
+  
+      //Consultar la api para obtener la cotizacion
+      const url =`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}` ;
+  
+      const resultado = await axios.get(url) 
+  
+      guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda])
+
+    }
+    cotizadarCriptomoneda()
+
+
+
+  }, [moneda , criptomoneda]) ;
+
+
+
+  return (
+    <Contenedor>
+      <div>
+        <Imagen 
+          src={imagen}
+          alt='Imagen Cripto' 
+        />
+
+      </div>
+      <div>
+        <Heading>
+          Cotiza Criptomonedas al instante
+        </Heading>
+
+        <Formulario
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+        />
+
+        <Cotizacion 
+          resultado={resultado}
+        />
+      </div>
+    </Contenedor>
+  );
+}
+
+export default App;
---
+++ src/hooks/useMoneda.jsx
@@ -0,0 +1,58 @@
+import React,{Fragment, useState} from 'react'
+import styled from '@emotion/styled';
+
+
+const Label = styled.label`
+    font-family:'Bebas Neues', cursive;
+    color: #FFF;
+    text-transform: uppercase ;
+    font-weight:bold ;
+    font-size: 2.4rem ;
+    margin-top: 2rem;
+    display: block; 
+`;
+
+
+const Select = styled.select`
+    width: 100%  ;
+    display: block;
+    padding: 1rem ;
+    -webkit-appearance: none ;
+    border-radius: 10px ;
+    border:none ;
+    font-size: 1.2rem ;
+
+`;
+
+
+
+
+const useMoneda = (label,stateInicial, opciones) => {
+
+    //STATE DE NUESTRO CUSTOM HOOK,  es el state de nuestro hook
+    const [state, actualizarState] = useState(stateInicial)
+    
+        const Seleccionar = () => (
+            <Fragment>
+                <Label > {label} </Label>
+                <Select 
+                    onChange={e => actualizarState(e.target.value)}   
+                    value={state} //Para que se cambie el state del hook 
+                >
+                    <option value="">-Seleccione-</option>
+                    {opciones.map(opcion =>(
+                        <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+
+                    ) )}
+                </Select>
+
+
+            </Fragment>
+
+        )
+
+        //Retornar state, intefaz y funcion que modifica el state
+        return [state, Seleccionar, actualizarState]
+}
+
+export default useMoneda;
